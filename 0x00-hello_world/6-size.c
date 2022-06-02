c877a9b26a12b4013fa8a105ca59a86764311491
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-02 14:38:09 -0700

    task 61

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	char chartype;
+	int inttype;
+	long int long_int_type;
+	long long int long_long_int_type;
+	float floattype;
+
+	printf("Size of a char: %zu byte(s)\n, sizeof(chartype));
+	printf("Size of an int: %zu byte(s)\n, sizeof(inttype));	
+	printf("Size of a long int: %zu byte(s)\n, sizeof(long_int_type));
+	printf("Size of a long long int: %zu byte(s)\n, sizeof(long_long_int_type));
+	printf("Size of a float: %zu byte(s)\n, sizeof(floattype));
+	retirn (0);
+}
