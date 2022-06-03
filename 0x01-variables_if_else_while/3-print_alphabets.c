09d50ad6e1b833f63b9f6e9a349d4daaaa9e5d3e
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-03 11:28:21 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; CH <= 'Z'; CH++)
+		putchar(CH);
+	putchar('\n');
+	return (0);
+}
