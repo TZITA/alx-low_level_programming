b54f4ecb83f63aaecaf5d609a4ac1ea1ca3a7975
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-03 16:45:53 -0700

    task666 fff

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,12 +9,9 @@
 int main(void)
 {
 	int i;
-	char ch;
 
 	for (i = 0; i < 10; i++)
 		putchar((i % 10) + '0');
-	for (ch = 'a'; ch <= 'f'; ch++)
-		putchar(ch);
 	putchar('\n');
 
 	return (0);
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-	int i;
-
+	int i;	
+	
 	for (i = 0; i < 10; i++)
 		putchar((i % 10) + '0');
 		putchar(' ');
