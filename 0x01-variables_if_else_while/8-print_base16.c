d6098e89aed9474302656ee295529af729ebe585
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-03 15:38:19 -0700

    task 8f8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,14 +8,20 @@
 
 int main(void)
 {
-	char CH = 'a';
-	int i = 0;
+	char CH,i;
 
-	for (i = 0; i < 10; i++)
+	i=0;
+
+	for(i = 0; i < 10; i++)
+	{
 		putchar(i);
-	for (CH = 'a'; CH <= 'f'; CH++)
-		putchar(CH);
-	putchar('\n');
+	}
 
+	for(CH = 'a'; CH <= 'f'; CH++)
+	{
+		putchar(CH);
+	}
+
+	putchar('\n');
 	return (0);
 }
