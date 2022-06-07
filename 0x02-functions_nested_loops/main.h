08930e7f78de3ed3bac9064b6b499b90cbecfccb
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-07 12:20:08 -0700

    task ooff

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,18 @@
-icc
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+		putchar('\n');
+}
+
