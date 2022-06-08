23f52c4319994e02f0512a7ac5ac78e36734d051
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-08 05:10:10 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - function declaration
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+		putchar(ch);
+		putchar('\n');
+		putchar(ch);
+		putchar('\n');
+		putchar(ch);
+		putchar('\n');
+		putchar(ch);
+		putchar('\n');
+		putchar(ch);
+		putchar('\n');
+		putchar(ch);
+		putchar('\n');
+		putchar(ch);
+		putchar('\n');
+		putchar(ch);
+		putchar('\n');
+		putchar(ch);
+	}
+
+	putchar('\n');
+}
