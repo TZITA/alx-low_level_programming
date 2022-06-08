ba3aae2410939b9f6c988dbbb0d4163901ec9ac9
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-08 11:14:10 -0700

    task 2final

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,22 +13,31 @@ void print_alphabet_x10(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
+	putchar('\n');
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
+	putchar('\n');
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
+	putchar('\n');
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
+	putchar('\n');
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
+	putchar('\n');
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
+	putchar('\n');
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
+	putchar('\n');
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
+	putchar('\n');
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
+	putchar('\n');
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 
