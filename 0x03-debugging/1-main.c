285bf7ed5e02525f52b28af08a2a7f533cfa2bbe
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-09 04:05:11 -0700

    task 11f

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+
 /**
  * main - causes an infinite loop
- *
  * Return: 0
  */
 
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop coming :(\n");
+	printf("Infinite loop incoming :(\n");
 /**
 *	i = 0;
 *
