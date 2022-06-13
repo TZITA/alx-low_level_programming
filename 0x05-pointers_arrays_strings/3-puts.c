3d59d6cf19e87aebd2860b25f6826714d0d317c6
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-13 09:41:23 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - puts strings to stdout
+ * @str: string
+ *
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	int str[i];
+	for(i = 0; i < strlen(str); i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
