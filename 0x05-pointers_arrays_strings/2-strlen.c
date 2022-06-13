f717ebbe637215b9542f7f1805b4a11ba7c43fbe
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-13 08:37:20 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - outputs the string length of a string
+ * @s: string
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	l = sizeof(*s) / 8;
+	return (l);
+}
