b587af1f96edd963b534bae619094e05ae15d443
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-13 14:19:00 -0700

    task7 qqqqqqq
    dslw;dkw

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: string
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int n;
+	int length_of_the_string = 10;
+
+	if (length_of_the_string % 2 == 0)
+	{
+		n = length_of_the_string / 2;
+		{
+			for (i = n; i < length_of_the_string; i++)
+				_putchar(str[i]);
+		}
+	}
+	else
+	{
+		n = (length_of_the_string - 1) / 2;
+		{
+			for (i = n; i < length_of_the_string; i++)
+				_putchar(str[i]);
+		}
+	}
+}
