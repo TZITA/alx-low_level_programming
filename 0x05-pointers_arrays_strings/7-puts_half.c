b90acd9ae2817f865b4b3da62da9fb934bcb76fc
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-13 14:21:55 -0700

    task 777

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,6 +19,7 @@ void puts_half(char *str)
 		{
 			for (i = n; i < length_of_the_string; i++)
 				_putchar(str[i]);
+				_putchar("\n");
 		}
 	}
 	else
@@ -27,6 +28,7 @@ void puts_half(char *str)
 		{
 			for (i = n; i < length_of_the_string; i++)
 				_putchar(str[i]);
+				_putchar("\n");
 		}
 	}
 }
