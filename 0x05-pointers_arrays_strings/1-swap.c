553cccc0d554f8548454ee7eb85af671f89bf00c
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-13 05:32:43 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the values of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = 98;
+}
