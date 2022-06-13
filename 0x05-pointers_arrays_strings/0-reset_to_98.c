8218c4d8fb095cb218720799d1b9642fa6883072
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-13 05:04:37 -0700

    task00

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
+/*
  * reset_to_98 - change n to 98
+ * n: is the value inside the pointer *n
  * Return: Always 0
  */
 
 void reset_to_98(int *n)
 {
-	/* #n is intiger */
 	*n = 98;
 }
