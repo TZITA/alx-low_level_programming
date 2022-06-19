202884013cf07746cf25f7a70cc5b5c5fbacda1f
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-19 01:39:38 -0700

    task102102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,5 +1,19 @@
-#include "main.h"
 #include <stdio.h>
+
+/**
+ * fibonacci - lists the fibonacci numbers
+ * @n: number of fibonacci
+ *
+ * Return: Always 0.
+ */
+
+int fibonacci(int n)
+{
+	if (n == 1 || n == 2)
+		return (1);
+	else
+		return (fibonacci(n -1) + fibonacci(n - 2));
+}
 
 /**
  * main -check the code
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,4 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _abs(int);
 int add(int, int);
-int fibonacci(int i);
 
