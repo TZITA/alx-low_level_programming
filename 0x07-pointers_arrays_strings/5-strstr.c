6d737ca009acfd19168226a2877b5a3b24283eba
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-20 05:53:23 -0700

    task 555

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,6 +2,28 @@
 #include <stdio.h>
 
 /**
+ * cpr - compares 2 strings
+ * @s1: string1
+ * @s2: string 2
+ *
+ * Return: True
+ */
+int cpr(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s2 == '\0');
+}
+
+
+/**
  * _strstr - locates a substring
  * @haystack: string
  * @needle: substring
@@ -13,7 +35,7 @@ char *_strstr(char *haystack, char *needle)
 {
 	while (*haystack != '\0')
 	{
-		if (*haystack == *needle)
+		if ((*haystack == *needle) && cpr(haystack, needle))
 		{
 			return (haystack);
 		}
