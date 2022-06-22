bd3ce2e8db9fac9b58292d76dbe2a70a3c34e224
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-22 13:24:11 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * pow_recursion - returns the value of a number raised to number
+ * @x: number
+ * @y: number
+ *
+ * Return: x raised to y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
