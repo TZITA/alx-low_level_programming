4d872025a4b0c0afb43ebb748a9055de5ce9d007
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-24 06:34:14 -0700

    task44

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,3 +3,29 @@
 
 /**
  * main - main function
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			sum = sum + *argv[i];
+			printf("%d\n", sum);
+		}
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
