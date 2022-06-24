0be0a11f5104b5113c3cb0810695237f75850f35
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-24 05:12:13 -0700

    task 3333

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,3 +3,5 @@ int _putchar(char c);
 int main(int argc __attribute__((unused)), *argv[]);
 int main(int argc, *argv[] __attribute__((unused)));
 int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+
