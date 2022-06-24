e868ce8f67bc61ccdf19be9931150999f0db27b3
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-24 03:36:46 -0700

    task11

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
