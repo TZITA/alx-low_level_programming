0c290649b0ebd8f16c1ad91b9072e058a574fa40
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-26 08:10:26 -0700

    t0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - entry point
+ * @size: siz
+ * @c: char
+ *
+ * Return: Char.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char __attribute__((unused)) *ptr;
+
+	ptr = (char*) malloc(sizeof(char) * size);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			_putchar(c);
+		}
+	}
+
+	return (0);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Tasks regarding memory allocation malloc and free!
