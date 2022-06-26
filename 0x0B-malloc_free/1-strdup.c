ca5c633a1548b06b74bc04805c4ee95a3728b334
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-26 12:34:17 -0700

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * strdup - entry point
+ * @str: string
+ *
+ * Return: Always 0.
+ */
+
+char *_strdup(char *str)
+{
+	int s_len;
+	char *str2;
+	char *s2_tmp;
+	
+	s_len = strlen(str);
+	str2 = (char *) malloc(sizeof(char) * s_len + 1);
+
+	if (str2 == NULL)
+	{
+		return (NULL);
+	}
+	
+	s2_tmp = str2;
+	while (*str)
+	{
+		*s2_tmp = *str;
+		s2_tmp++;
+		str++;
+	}
+	*s2_tmp = '\0';
+	return (str2);
+}
