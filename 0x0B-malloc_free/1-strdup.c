070808c0a40c20f146500c929a31007cab974bca
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-28 06:17:26 -0700

    task1please

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,9 +19,9 @@ char *_strdup(char *str)
 	s_len = strlen(str);
 	str2 = (char *) malloc(sizeof(char) * s_len + 1);
 
-	if (str2 == NULL)
+	if (str2 == 0)
 	{
-		return ('\0');
+		return (NULL);
 	}
 
 	s2_tmp = str2;
