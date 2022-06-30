0d6e79f8e65592d6b825e144cd0bd93877a1db9a
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-30 13:49:44 -0700

    t101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number
+ * @argv: args
+ *
+ * Return: N1xn2.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int mult;
+
+	argc = 3;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mult = atoi(argv[1]) * atoi(argv[2]);
+		}
+		printf("%d\n", mult);
+	}
+	return (0);
+}
