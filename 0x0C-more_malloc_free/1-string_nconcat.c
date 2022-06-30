1e3f70a45273598a0864f22383c44ee247117c83
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-30 08:40:09 -0700

    t11f

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,15 +36,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			ptr[i] = s1[i];
 		}
 	}
-	for (i = s1_len; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (s2 == NULL)
 		{
-			ptr[s1_len] = ' ';
+			ptr[i] = ' ';
 		}
 		else
 		{
-			ptr[i] = s2[i];
+			ptr[s1_len + i] = s2[i];
 		}
 	}
 	return (ptr);
