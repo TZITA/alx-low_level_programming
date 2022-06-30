370da328c452f11b867bb8e251df47c397b7931e
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-30 12:41:23 -0700

    t3f3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,4 +3,24 @@
 #include <stdlib.h>
 
 /**
+ * array_range - entry point
+ * @min: minumum
+ * @max: maximum
  *
+ * Return: 0.
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+	int diff = max - min + 1;
+	int *ptr;
+
+	ptr =(int *) malloc(sizeof(int) * diff);
+
+	for (i = 0; i <= diff; i++)
+	{
+		ptr[i] = min + i;
+	}
+	return (ptr);
+}
