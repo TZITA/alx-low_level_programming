81214139777070cbd24b00e59ceaf3a79aa843c7
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-06-30 09:13:23 -0700

    t22f

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,4 +3,24 @@
 #include <stdio.h>
 
 /**
+ * _calloc - entry p
+ * @nmemb: mem
+ * @size: int
  *
+ * Return: 0.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	ptr = malloc(nmemb * size);
+	if (ptr == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (ptr);
+	}
+}
