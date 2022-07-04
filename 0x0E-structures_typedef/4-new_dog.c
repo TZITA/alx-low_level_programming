3218d09773838b34f8f1544005a55bebd5a73c00
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-04 13:13:01 -0700

    t4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,3 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
+/**
+ * new_dog - mm
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: dog_t
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(dog_t));
+	if (!ptr)
+	{
+		return (NULL);
+	}
+
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
+
+	return (ptr);
+}
