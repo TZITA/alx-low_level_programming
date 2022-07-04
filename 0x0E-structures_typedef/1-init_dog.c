0ce18fdbcc22428b89cd1f9fcc4cb50c8561824e
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-04 14:17:58 -0700

    tall

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,10 +15,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *ptr;
-
-	ptr = malloc(sizeof(struct dog));
-	ptr->name = name;
-	ptr->age = age;
-	ptr->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
