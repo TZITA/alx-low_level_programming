50754b42f609b40ec914c4ade677899020b761b5
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-04 07:34:01 -0700

    t0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog's name age owner
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: Dog type
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
