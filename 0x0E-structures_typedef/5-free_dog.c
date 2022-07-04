b0d946bf89d425910de2c343548d7fbdf8ca59ed
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-04 14:11:40 -0700

    tt

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
+
+/**
+ * free_dog - entry
+ * @d: dogg
+ *
+ * Return: Nothing.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
