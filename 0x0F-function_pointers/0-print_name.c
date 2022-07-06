57620bfd24c5628bb3915fa5924192dc25830c4d
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-06 06:04:48 -0700

    update1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers"
+
+/**
+ * print_name - entry point
+ * @name: name
+ * @f: pointer
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(char *);
+}
