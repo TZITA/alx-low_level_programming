f71b546635da0bd919d4a23abe48bee7c6d6d017
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-06 13:11:07 -0700

    t3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: Always 0.
+ */
+
+int main(int *argc, char *argv[])
+{
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	if (argv[2] != "+" || !argv[2] = "-" || argv[2] != "*" || argv[2] != "/" || argv[2] != "%")
+	{
+		printf("Error\n");
+		return (99);
+	}
+	if ((argv[2] = "/" || argv[2] = "%") && arvgv[3] = 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - entry point
+ * @a: int
+ * @b: int
+ *
+ * Return: Int.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - entry point
+ * @a: int
+ * @b: int
+ *
+ * Return: Int.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - entry point
+ * @a: int
+ * @b: int
+ *
+ * Return: Int.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - entry point
+ * @a: int
+ * @b: int
+ *
+ * Return: Int.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - entry
+ * @a: int
+ * @b: int
+ *
+ * Return: Int.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
