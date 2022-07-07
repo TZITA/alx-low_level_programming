9c806b222f0e290e445f481be17af1dbdc200e5d
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-07 03:19:43 -0700

    t0f

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * sum_them_all - entry point
+ * @n: integer
+ *
+ * Return: Int.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+
+	va_list p;
+
+	va_start(p, n);
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(p, int);
+	}
+	va_end(p);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Tasks regarding veriadic functions
