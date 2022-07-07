3f1f3f5632a7bdca4c94837e73e208a167247342
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-07 14:25:25 -0700

    t1finala

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,16 +20,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != n - 1)
-		{
-			val = va_arg(p, int);
-			printf("%d", val);
-			printf("%s", separator);
-		}
-		else
-		{
-			printf("%d", val);
-		}
+		val = va_arg(p, int);
+		printf("%d", val);
+		printf("%s", separator);
 	}
 	printf("\n");
 	va_end(p);
