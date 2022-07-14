bcfc19c41834f5531807748aa9730e3baae167ba
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-14 15:46:08 -0700

    tyf

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,8 +5,8 @@
  * Return: Nothing.
  */
 
-void firstPrint() __attribute__((constructor));
-void firstPrint()
+void firstPrint(void)__attribute__((constructor));
+void firstPrint(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
