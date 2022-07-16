50558773474e713ab8795491930f0894f3c5dc59
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-16 09:49:32 -0700

    rev65

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - entry point
+ * @c: int
+ *
+ * Return: Int.
+ */
+
+int _isalpha(int c)
+{
+	int i;
+	
+	for 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,5 +7,6 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _abs(int);
 int add(int, int);
+int print_sign(int n);
 
 #endif
