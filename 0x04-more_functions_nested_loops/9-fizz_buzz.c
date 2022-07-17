627ecf579bd3143b5beebf003dfe29c6462f9ab3
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-17 13:19:50 -0700

    trev9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,11 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("%s", "Fizz");
 		}
