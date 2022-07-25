98dfead551363a3813934a3361025d93ee0c9b6a
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-25 13:04:24 -0700

    t2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,1 +1,34 @@
 #include "main.h"
+#include <string.h>
+
+/**
+ * append_text_to_file - entry point
+ * @filename: file name
+ * @text_content: text content
+ *
+ * Return: Int.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fileoo;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fileoo = open(filename, O_WRONLY | O_APPEND, 0334);
+
+	if (fileoo == -1)
+	{
+		return (-1);
+	}
+	else if (text_content == NULL)
+	{
+		write(fileoo, 0, 0);
+	}
+
+	write(fileoo, text_content, strlen(text_content));
+	return (1);
+}
