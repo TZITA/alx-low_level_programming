5165a688b9e98af0f1df96c3ae8b55ea48283774
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-25 12:31:51 -0700

    t11

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,14 +18,13 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fileoo = open("filename", O_CREAT | O_RDWR, 0600);
+	fileoo = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+
 	if (fileoo == -1)
 	{
 		return (-1);
 	}
 
 	write(fileoo, text_content, strlen(text_content)); 
-
-	close(fileoo);
 	return (1);
 }
