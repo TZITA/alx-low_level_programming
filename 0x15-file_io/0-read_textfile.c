1d8574210a6c111357a1ec790331c550695c6e91
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-25 08:20:54 -0700

    t0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - entry point
+ * @filename: file name
+ * @letters: number
+ *
+ * Return: Number.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fileo;
+	int filer;
+	char *c = malloc(sizeof(char) * letters);
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fileo = open("filename", O_RDONLY);
+	if (fileo < 0)
+	{
+		return (0);
+	}
+
+	filer = read(fileo, c, letters);
+	write(filer, c, letters);
+	if (letters == 0)
+	{
+		return (0);
+	}
+	return (filer);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+Readme about File I/O tasks
