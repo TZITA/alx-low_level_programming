b340466980d998783e0c032baff73dea350de5df
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-25 11:45:48 -0700

    t1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,31 @@
 #include "main.h"
+#include <string.h>
+
+/**
+ * create_file - entry point
+ * @filename: file name
+ * @text_content: text content
+ *
+ * Return: 1 or -1.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fileoo;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fileoo = open("filename", O_CREAT | O_RDWR, 0600);
+	if (fileoo == -1)
+	{
+		return (-1);
+	}
+
+	write(fileoo, text_content, strlen(text_content)); 
+
+	close(fileoo);
+	return (1);
+}
