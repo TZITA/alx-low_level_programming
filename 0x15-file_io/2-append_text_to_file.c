0c50c9d45e3641b0929e588c05e868e772f06446
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-26 06:36:50 -0700

    mod 1

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,6 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	else if (text_content == NULL)
 	{
 		write(fileoo, 0, 0);
+		return (1);
 	}
 
 	write(fileoo, text_content, strlen(text_content));
