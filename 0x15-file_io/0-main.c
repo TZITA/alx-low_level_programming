7f4473e4968812a60a7e15c81e9a554d60ba083b
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-26 07:46:13 -0700

    tmod33

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -38,10 +38,10 @@ int main(int ac, char **av)
 
 		        printf("\n(printed chars: %li)\n", n);
 
-			    n = read_textfile(av[1], 1024);
+/**			    n = read_textfile(av[1], 1024);
 
 			        printf("\n(printed chars: %li)\n", n);
-
+*/
 				    return (0);
 
 }
