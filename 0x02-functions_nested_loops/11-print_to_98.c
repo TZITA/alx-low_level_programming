4014cbd3e9d2fd58572b8da281d44dad2b16fd33
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-26 10:15:56 -0700

    t44445

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,10 +11,21 @@
 void print_to_98(int n)
 {
 	int i;
-
-	for (i = n; i < 98; i++)
+	
+	if (n < 98)
 	{
-		printf("%d, ", i);
+		for (i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("%d\n", 98);
 	}
-	printf("%d\n", 98);
+	else if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+		printf("%d\n", 98);
+	}
 }
