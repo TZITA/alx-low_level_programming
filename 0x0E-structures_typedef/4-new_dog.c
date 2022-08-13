46feb6505dc65e68f9c9e0614d04e2889cf7b608
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-08-13 13:00:13 -0700

    tver

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,8 +21,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 	{
-		free(ptr);
 		return (NULL);
+		free(ptr);
 	}
 	dup_name = strdup(name);
 	dup_owner = strdup(owner);
