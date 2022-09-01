7b346d80826d9c3f95d5631d4e0436c9a84e3459
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-09-01 13:49:40 -0700

    t7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - e p
+ * @h: head
+ * @idx: index
+ * @n: data
+ *
+ * Return: Address of new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new;
+	dlistint_t *tmp;	
+	unsigned int i;
+
+	new = malloc(sizeof(dlistint_t));
+	new->n = n;
+	tmp = *h;
+	for (i = 0; i < idx - 1; i++)
+	{
+		tmp = tmp->next;
+	}
+	new->next = tmp->next;
+	tmp->next = new;
+	new->prev = tmp->prev;
+	if (new->next != NULL)
+	{
+		new->next->prev = new;
+	}
+	return (new);
+}
