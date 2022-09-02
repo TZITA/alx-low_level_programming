c63811fb40583b4318e27b4eceeb1bc7b6064ef4
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-09-02 05:25:12 -0700

    t3rev

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,6 +14,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *end = *head;
 
 	new = malloc(sizeof(dlistint_t));
+	if new == NULL
+	{
+		return (NULL);
+		free(head);
+	}
 	new->n = n;
 	new->next = NULL;
 
