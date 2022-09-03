34c408c18ab4acba525de40cc39942d150fbc726
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-09-03 06:57:29 -0700

    dd

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * time_table - e p
+ *
+ * Return: Nothing
+ */
+
+void times_table(void)
+{
+		int i;
+		int j;
+		int p;
+
+		for (i = 0; i < 10; i++)
+		{
+			_putchar('0');
+			for (j = 1; j < 10; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				p = i * j;
+				if (p < 10)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(p / 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+}
