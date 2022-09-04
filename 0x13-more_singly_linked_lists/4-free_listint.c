203ff7c44712156c4973c0608ab3a2913c76ec11
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-09-04 07:09:00 -0700

    tf

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -11,10 +11,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *tmp;
 
-	while (head->next != NULL)
+	while (head)
 	{
-		tmp = head;
-		head = head->next;
-		free(tmp);
+		tmp = head->next;
+		free(head);
+		head = tmp;
 	}
 }
