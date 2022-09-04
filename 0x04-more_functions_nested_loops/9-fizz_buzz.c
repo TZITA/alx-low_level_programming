6407abd8d9a9689b25022690fde68f77e48e0eb0
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-09-04 01:02:44 -0700

    dfs

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,24 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("%s", "FizzBuzz");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("%s", "Fizz");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("%s", "Buzz");
+			printf("Buzz");
 		}
 		else
 		{
 			printf("%d", i);
+		}
+
+		if (i == 100)
+		{
+			continue;
 		}
 		printf(" ");
 	}
