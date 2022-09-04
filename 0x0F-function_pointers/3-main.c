cf9b77c92d3c01e54633169d04655a2d796edf04
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-09-04 09:45:04 -0700

    ff

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,30 +7,30 @@
  * Return: Always 0.
  */
 
-int main(int *argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int a = atoi(argv[1]);
-	int b = atoi(argv[3])
-	char *p;
-	int final;
+	int a, b;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
-	if (argv[2] != "+" || !argv[2] = "-" || argv[2] != "*" || argv[2] != "/" || argv[2] != "%")
+	a = atoi(argv[1]);
+	op = argv[2];
+	b = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		return (99);
+		exit(99);
 	}
-	if ((argv[2] == "/" || argv[2] == "%") && argv[3] == 0)
+	if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
 	{
 		printf("Error\n");
-		return (100);
+		exit(100);
 	}
-	
-	p = get_op_func(argv[1]);
-	
+	printf("%d\n", get_op_func(op)(a, b));
 	return (0);
 }
