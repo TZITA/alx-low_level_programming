1488d995a8aebbc3c7c91688d20187be1ed75c33
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-09-04 00:50:09 -0700

    ff

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - ep
+ * @size: size
+ *
+ * Return: Nothing.
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			if j == size - 1;
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
