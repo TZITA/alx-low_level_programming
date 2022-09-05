bbddb2be5fdc9c344d1eca078db57e4616d93ae8
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-09-05 12:40:54 -0700

    t2

---
+++ 0x18-dynamic_libraries/11-main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * add - ep
+ * @a: n
+ * @b: n
+ *
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - ep
+ * @a: n
+ * @b: n
+ *
+ * Return: a - b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - ep
+ * @a: n
+ * @b: n
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - ep
+ * @a: n
+ * @b: n
+ *
+ * Return: a / b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - ep
+ * @a: n
+ * @b: n
+ *
+ * Return: a % b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
