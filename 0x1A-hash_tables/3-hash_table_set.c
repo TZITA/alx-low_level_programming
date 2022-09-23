dd95f5c4825f4869598a00518f413e31a2984c02
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-09-23 06:25:28 -0700

    t3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	
+
 	cpy = strdup(value);
 	if (cpy == NULL)
 		return (0);
