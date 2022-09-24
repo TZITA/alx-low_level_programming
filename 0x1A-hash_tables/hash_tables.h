86fce9da125a70d15506334aad947deeb70a03df
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-09-24 00:37:12 -0700

    trev

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -6,7 +6,7 @@
 #include <string.h>
 
 /**
- * struct_hash_node_s - Node of a hash table
+ * struct hash_node_s - Node of a hash table
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
