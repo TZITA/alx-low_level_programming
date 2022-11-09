1c75203ac738f302614e30829489fb47a6649fd4
Thomas Beyene <thomas.beyene.@gmail.com>
2022-11-09 23:53:01 +0300

    t5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,10 +9,15 @@ def island_perimeter(grid):
     width = len(grid[0])
     height = len(grid)
     cnt = 0
+    edges = 0
+
 
     for i in range(height):
         for j in range(width):
             if grid[i][j] == 1:
                 cnt += 1
-
-    return cnt * 2 + 2
+                if (j > 0 and grid[i][j + 1] == 1):
+                    edge += 1
+                if (i > 0 and grid[i + 1][j] == 1):
+                    edge += 1
+    return cnt * 4 - not_edge * 2
