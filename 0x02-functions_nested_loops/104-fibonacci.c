70b031385d14ad239ab8d964ae773f8fad767bb8
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-11-25 18:19:52 +0300

    rev2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,47 +5,24 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	unsigned long int a = 1;
 	unsigned long int b = 2;
 	unsigned long int c;
-	unsigned long int h1, h2;
-	unsigned long int ah1, ah2, bh1, bh2;
 	int i;
 
-	for (i = 0; i < 92; i++)
+	for (i = 0; i < 98; i++)
 	{
-		c = a + b;
-		printf("%lu", c);
-		a = b;
-		b = c;
+		if (i < 97)
+		{
+			c = a + b;
+			printf("%lu, ", a);
+			a = b;
+			b = c;
+		}
+		printf("%lu", a);
 	}
-	ah1 = a / 10000000000;
-	bh1 = b / 10000000000;
-	ah2 = a % 10000000000;
-	bh2 = b % 10000000000;
 
-	for (i = 93; i < 99; i++)
-	{
-		h1 = ah1 + bh1;
-		h2 = ah2 + bh2;
-		if (ah2 + bh2 > 9999999999)
-		{
-			h1 = h1 + 1;
-			h2 = h2 % 10000000000;
-		}
-		printf("%lu%lu", h1, h2);
-		if (i != 98)
-		{
-			printf(", ");
-		}
-		ah1 = bh1;
-		ah2 = bh2;
-		bh1 = h1;
-		bh2 = h2;
-	}
-	printf("\n");
 	return (0);
 }
