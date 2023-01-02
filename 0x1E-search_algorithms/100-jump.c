55f088e0c8927228d21dff97b454da296a61c392
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-01-02 17:14:58 +0300

    tt

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * jump_search -  searches for a value using the Jump search algorithm
+ * @array: Array to be searched
+ * @size: Number of elements in array
+ * @value: Value to search for in array
+ *
+ * Return: Index where value is located.
+ */
+int jump_search(int *array, size_t size, int value)
+{
+
