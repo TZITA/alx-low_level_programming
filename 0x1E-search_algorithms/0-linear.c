d53c829b13ba65b8c4edb244d26c2dae68c7f727
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-01-02 15:27:03 +0300

    Task 1: Linear Search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,33 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value using the Linear search algorithm
+ * @array: Array to be searched
+ * @size: Number of elements in the array
+ * @value: Value to be searched for in the array
+ *
+ * Return: The first index where value is located.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			return (i);
+		}
+		else
+		{
+			printf("Value checked array[%ld] = [%d]", i, array[i]);
+			printf("\n");
+		}
+	}
+	return (-1);
+}
