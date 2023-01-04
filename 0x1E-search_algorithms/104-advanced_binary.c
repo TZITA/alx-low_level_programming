2e60db6a0176ea1202da3f0a3017bf1cf1aefa08
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-01-04 18:29:19 +0300

    t103

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - Improved version of binary search
+ * @array: Array to be searched
+ * @size: Number of elements in array
+ * @value: Value to be searched in array
+ *
+ * Return: The index of the first index of the value located.
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+
