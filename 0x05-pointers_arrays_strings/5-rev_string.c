c210d5b8e76accbb50017cdd6570268be87b8b52
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-06-15 16:51:02 +0300

    Reverses a string in place

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * rev_string - reverses a string
@@ -10,7 +9,16 @@
 void rev_string(char *s)
 {
 	int i;
+	char temp;
+	int len = 0;
 
-	for (i = 9; i >= 0; i--)
-		_putchar(s[i]);
+	while (s[len] != '\0')
+		len++;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
 }
