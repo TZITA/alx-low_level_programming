c77a0ed8f04eaee4555dde4640ffef5769278960
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-06-15 19:03:51 +0300

    Handled to NULL struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,21 +4,15 @@
 
 /**
  * init_dog - entry point
- * @d: mmm
- * @name: mem2
- * @age: mem3
- * @owner: mem4
- *
- * Return: Always 0.
+ * @d: dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-	{
-		exit(0);
-	}
-	else
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
