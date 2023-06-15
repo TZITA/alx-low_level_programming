956c3a5bdfabab51239c9d4bb26880665660d42e
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-06-15 20:10:39 +0300

    handled malloc failure

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,20 +11,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	char *n;
+	list_t *new_node = malloc(sizeof(list_t));
 
-	if (head == NULL)
+	if (head == NULL && new_node == NULL)
 	{
 		return (NULL);
 	}
-	else
-	{
-		list_t *new_node = malloc(sizeof(list_t));
 
-		n = strdup(str);
-		new_node->str = n;
-		new_node->len = strlen(str);
-		new_node->next = *head;
-		*head = new_node;
-	}
+	n = strdup(str);
+	new_node->str = n;
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
 	return (*head);
 }
