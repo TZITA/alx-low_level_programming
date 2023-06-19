eff509b6ddf2758fb9d7b9c364a327f54030d35a
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-06-19 22:14:41 +0300

    check code

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,21 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
+ * print_listint - entry point
+ * @h: Struct
  *
+ * Return: number of nodes
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	int cnt = 0;
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *) &head, head->n);
+		head = head->next;
+		cnt++;
+	}
+	return (cnt);
+}
